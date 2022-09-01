34355113792d98e9f70538f69399da79737ebe5e
abraham <apolichenco@gmail.com>
2022-09-01 14:12:37 -0400

    making navlinks

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,35 @@
 import React, { useState, useEffect } from "react";
-import Main from "./Main"
-import Header from "./Header"
-
-
+import Calendar from "./Calendar";
+import Header from "./Header";
+import EventsList from "./EventsList"
+import AddEventForm from "./AddEventForm"
+import ReactDOM from "react-dom";
+import { Route, Switch} from "react-router-dom"
+ 
 function App() {
+  const [days, setDays] = useState([])
+  
+  useEffect (() => {
+    fetch("https://www.hebcal.com/hebcal?v=1&cfg=json&maj=on&min=on&mod=off&nx=on&start=2022-09-25&end=2022-10-25&month=x&ss=on&mf=on&c=off&geo=zip&zip=33065&m=0&s=on&leyning=off&d=on&o=on&i=off")
+    .then((r) => r.json())
+    .then((data) => setDays(data.items))
+  }, [])
+
 
   return (
-    <div className="App">
-      <h1>Tishrei</h1>
+    <div>
       <Header />
-      <Main />
+      <Switch>
+        <Route path="/events">
+          <EventsList />
+        </Route>
+        <Route path="/add-event">
+          <AddEventForm />
+        </Route>
+        <Route path="/">
+          <Calendar daysData={days}/>
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,50 @@
 import React, { useState, useEffect } from "react";
-
+import {NavLink} from "react-router-dom"
 
 function Header() {
+
+  const linkStyles = {
+    display: "inline-block",
+    width: "50px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "blue",
+    textDecoration: "none",
+    color: "white",
+  };
+
   return (
     <div>
+      <NavLink 
+      to="./"
+      exact
+      style={linkStyles}
+      activeStyle={{
+        background: "darkblue"
+      }}
+      >
+        Calendar
+      </NavLink>
+      <NavLink 
+      to="./events"
+      exact
+      style={linkStyles}
+      activeStyle={{
+        background: "darkblue"
+      }}
+      >
+        Events List
+      </NavLink>
+      <NavLink 
+      to="./add-event"
+      exact
+      style={linkStyles}
+      activeStyle={{
+        background: "darkblue"
+      }}
+      >
+        Add Event
+      </NavLink>
     </div>
   );
 }

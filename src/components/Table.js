918ffa7d8fd0bcb21a5dcb9be3fc277e1c719bac
abraham <apolichenco@gmail.com>
2022-09-01 09:15:03 -0400

    added react calendar to my page

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,49 @@
 import React, { useState, useEffect } from "react";
 import Day from "./Day";
+import Calendar from 'react-calendar';
 
 
-function Table() {
+function Table({daysData}) {
+  const [hebdate, setHebdate] = useState([])
+  const [holiday, setHoliday] = useState([])
+  const [candles, setCandles] = useState([])
+  const [zmanim, setZmanim] = useState([])
+  const [parashat, setParashat] = useState([])
+  const [roshchodesh, setRoshchodesh] = useState([])
+  const [date, setDate] = useState(new Date());
+
+  useEffect (() => {
+    setHebdate(daysData.filter((day) => day.category === "hebdate"))
+  }, [])
+  useEffect (() => {
+    setHoliday(daysData.filter((day) => day.category === "holiday"))
+  }, [])
+  // useEffect (() => {
+  //   setCandles(daysData.filter((day) => day.category === "candles"))
+  // }, [])
+  // useEffect (() => {
+  //   setZmanim(daysData.filter((day) => day.category === "zmanim"))
+  // }, [])
+  useEffect (() => {
+    setParashat(daysData.filter((day) => day.category === "parashat"))
+  }, [])
+  useEffect (() => {
+    setRoshchodesh(daysData.filter((day) => day.category === "roshchodesh"))
+  }, [])
+
+
   return (
-    <div>
-      <table>
+    <div className='app'>
+          
+      <h1 className='text-center'>React Calendar</h1>
+      <div className='calendar-container'>
+        <Calendar onChange={setDate} value={date} />
+      </div>
+      <p className='text-center'>
+        <span className='bold'>Selected Date:</span>{' '}
+        {date.toDateString()}
+      </p>
+      {/* <table>
         <tr>
           <th>Sunday</th>
           <th>Monday</th>
@@ -30,7 +68,7 @@ function Table() {
         <tr>
           <Day />
         </tr>
-      </table>
+      </table> */}
     </div>
   );
 }

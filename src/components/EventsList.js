c906f7eba99566d8c6751545b8ffef37fbc8ad07
abraham <apolichenco@gmail.com>
2022-09-01 22:52:52 -0400

    finished making the list

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -3,18 +3,21 @@ import React, { useState, useEffect } from "react";
 
 function EventList({daysData}) {
   const [bigDays, setBigDays] = useState([])
-  // const [parashat, setParashat] = useState([])
 
   useEffect (() => {
-    setBigDays(daysData.filter((day) => day.category === "holiday", "parashat"))
+    setBigDays(daysData.filter((day) => day.category === "holiday" || day.category === "parashat"))
   }, [daysData])
-  // useEffect (() => {
-  //   setParashat(daysData.filter((day) => day.category === "parashat"))
-  // }, [daysData])
-  
+
   return (
     <div>
-      
+      {bigDays.map((day, index) => {
+         const noYear = day.hdate.slice(0, day.hdate.length - 4);
+        return (
+          <div key={index}>
+            <h2>{day.title}</h2> 
+            <h4>{noYear}</h4>
+          </div>)
+      })}
     </div>
   );
 }
